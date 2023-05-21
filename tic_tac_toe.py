2f917c0218b4dff9da30759e0326911805e3ae28
lanesummer <17557428+lanesummer@users.noreply.github.com>
2023-05-21 10:52:31 -0700

    create title for tic tac toe game
    Create title for tic tac toe game using block letters. Import the title to tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,2 +1,11 @@
 # tic-tac-toe game
 
+# imports
+
+from tic_tac_toe_title import title
+
+
+
+
+# print title of game
+print(title)
---
+++ tic_tac_toe_title.py
@@ -0,0 +1,25 @@
+# tic-tac-toe game title
+
+# create block letters to create the title
+tic_tac = '''
+ TTTTT  IIIII   CCC          TTTTT    A     CCC
+   T      I    C   C           T     A A   C   C
+   T      I    C      *****    T    A   A  C
+   T      I    C      *****    T    AAAAA  C
+   T      I    C               T    A   A  C
+   T      I    C   C           T    A   A  C   C
+   T    IIIII   CCC            T    A   A   CCC
+  '''
+toe = '''
+              TTTTT   OOO   EEEEE
+                T    O   O  E
+                T    O   O  E
+                T    O   O  EEE
+                T    O   O  E
+                T    O   O  E
+                T     OOO   EEEEE
+'''
+title_line = '''
+*************************************************
+'''
+title = title_line + tic_tac + toe + title_line
